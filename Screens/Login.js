20fe19fdda4685b9640e6484b1b624487b625ede
internet_trash <naiknischal92@gmail.com>
2024-01-22 18:57:14 +0530

    added firebase and eas

--- Screens/Login.js
+++ Screens/Login.js
@@ -1,12 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text , StyleSheet} from 'react-native'
+import React from 'react';
+
+
+
+
 
 const Login = () => {
+  
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+     <Text style={styles.text}>Login</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Login
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 50,
+  },
+});
+
+export default Login;
